fc529b9f07bfee9cad006b2a9f2eea2ca0913720
Arsyil1737 <lelya.rahmat@gmail.com>
2025-08-31 08:31:11 +0700

    Create color_LED.js
    the rainbow LED

---
+++ mods/color_LED.js
@@ -0,0 +1,61 @@
+elements.r_LED = {
+    color: "rgb(64,0,0)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+elements.o_LED = {
+    color: "rgb(64,32,0)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+elements.y_LED = {
+    color: "rgb(64,64,0)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+elements.g_LED = {
+    color: "rgb(0,64,0)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+elements.b_LED = {
+    color: "rgb(0,0,64)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+elements.p_LED = {
+    color: "rgb(32,0,64)",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
+    conduct: 1,
+    insulate: false,
+    light: 10,
+}
+
+
